dce1afa110c4bc2412078e549dff88c13e31d6bd
Charlie Chau <charlie.chau@ihealthlabs.com>
2018-12-02 20:51:08 +0800

    Stage 4 proposal meant accepted It is included in preset-env

--- .babelrc.js
+++ .babelrc.js
@@ -4,31 +4,28 @@ module.exports = {
   presets: [['@babel/env', { modules: false }], '@babel/preset-react'],
   plugins: [
     // Stage 0
-    // "@babel/plugin-proposal-function-bind",
+    // '@babel/plugin-proposal-function-bind',
 
     // Stage 1
-    // "@babel/plugin-proposal-export-default-from",
-    // "@babel/plugin-proposal-logical-assignment-operators",
-    // ["@babel/plugin-proposal-optional-chaining", { loose: false }],
-    // ["@babel/plugin-proposal-pipeline-operator", { proposal: "minimal" }],
-    // ["@babel/plugin-proposal-nullish-coalescing-operator", { loose: false }],
+    // '@babel/plugin-proposal-export-default-from',
+    // '@babel/plugin-proposal-logical-assignment-operators',
+    // ['@babel/plugin-proposal-optional-chaining', { loose: false }],
+    // ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
+    // ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: false }],
     '@babel/plugin-proposal-do-expressions',
 
     // Stage 2
     ['@babel/plugin-proposal-decorators', { legacy: true }],
-    // "@babel/plugin-proposal-function-sent",
-    // "@babel/plugin-proposal-export-namespace-from",
-    // "@babel/plugin-proposal-numeric-separator",
-    // "@babel/plugin-proposal-throw-expressions",
+    // '@babel/plugin-proposal-function-sent',
+    // '@babel/plugin-proposal-export-namespace-from',
+    // '@babel/plugin-proposal-numeric-separator',
+    // '@babel/plugin-proposal-throw-expressions',
 
     // Stage 3
     '@babel/plugin-syntax-dynamic-import', // babel needs this for parsing but won't touch the syntax --> webpack will handle
-    // "@babel/plugin-syntax-import-meta",
+    // '@babel/plugin-syntax-import-meta',
     ['@babel/plugin-proposal-class-properties', { loose: false }],
-    // "@babel/plugin-proposal-json-strings",
-
-    // Stage 4
-    ['@babel/proposal-object-rest-spread'],
+    // '@babel/plugin-proposal-json-strings',
 
     ['@babel/transform-runtime', { useESModules: es }],
     'react-require',
